8394c70cb471e0ca12ed742c40995940d123450d
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-09 14:20:24 -0200

    Adde MoveOffSet script

---
+++ Assets/Scripts/MoveOffset.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOffset : MonoBehaviour {
+
+	private Material currentMaterial;
+	public float speed;
+	private float offset;
+
+	private GameController gameController;
+
+	void Start () {
+		currentMaterial = GetComponent<Renderer>().material;
+
+		gameController = FindObjectOfType (typeof(GameController)) as GameController;
+	}
+
+	void Update () {
+
+		if (gameController.GetCurrentState () != GameStates.INGAME) 
+		{
+			return;
+		}
+	
+		offset += 0.001f;
+
+		currentMaterial.SetTextureOffset ("_MainTex", new Vector2 (offset * speed, 0));
+
+	}
+}
