22fe377be07f637f380171bdc572da8190e23dbb
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-09 10:07:32 -0200

    Implement RigidBody in Player

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
+	public Transform mesh;
+	public float forceFly;
+	private Animator animatorPlayer;
+
+	private float currentTimeToAnim;
+	private bool inAnim;
+
 	void Start () {
-	
+		animatorPlayer = mesh.GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			inAnim = true;
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, 1) * forceFly);
+		}
+
+		if (inAnim) 
+		{
+			currentTimeToAnim += Time.deltaTime;
+
+			if (currentTimeToAnim > 0.2f) 
+			{
+				currentTimeToAnim = 0;
+				inAnim = false;
+			}
+		}
+
+		animatorPlayer.SetBool ("callFly", inAnim);
 	
 	}
 }
