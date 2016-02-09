a55b69bd249b5078e15fb544b2e83340b78d1d40
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-09 12:08:03 -0200

    Add Script ObstablesBehaviour

---
+++ Assets/Scripts/ObstaclesBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclesBehaviour : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+		transform.position += new Vector3 (speed, 0, 0) * Time.deltaTime;
+
+		if (transform.position.x < -5) {
+			gameObject.SetActive (false);
+		}
+	}
+}
