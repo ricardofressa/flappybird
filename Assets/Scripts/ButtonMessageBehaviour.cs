fee9c401f9353b92becf7777d10c4263be836190
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-13 17:35:33 -0200

    Finalized GameOver Input Button Ok

---
+++ Assets/Scripts/ButtonMessageBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class ButtonMessageBehaviour : MonoBehaviour {
+
+	public GameObject target;
+	public string message;
+
+	void Start ()
+	{
+		GetComponent<PressGesture> ().StateChanged += HandleStateChanged;
+	}
+
+	void HandleStateChanged (object sender, GestureStateChangeEventArgs e)
+	{
+		if (e.State == Gesture.GestureState.Ended) {
+			target.SendMessage (message, SendMessageOptions.RequireReceiver);
+		}
+	}
+	void Update ()
+	{
+	
+	}
+}
