d17db482aa04ca9ba0fc12ccd6cca33890c60e42
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-16 23:12:31 -0200

    Implementing button pause

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+
+	public Texture2D pauseImage;
+	public Texture2D startImage;
+
+	private GameController gameController;
+
+	public float sizeButton;
+
+	private bool isPaused;
+
+
+	void Start () 
+	{
+
+		gameController = FindObjectOfType (typeof(GameController)) as GameController;
+	
+	}
+	
+	void OnGUI()
+	{
+		if (gameController.GetCurrentState () != GameStates.INGAME)
+		{
+			if (!isPaused) 
+			{
+				if (GUI.Button (new Rect (0, 0, sizeButton, sizeButton), pauseImage)) 
+				{
+					isPaused = true;
+				}
+			}
+		}
+	}
+}
